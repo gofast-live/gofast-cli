64c0a45d2e7fec8b11b548fcbc5103d502f13b3e
mateuszpiorowski <mateuszpiorowski@gmail.com>
2025-07-12 09:36:26 +0200

    update

---
+++ cmd/gofastv2/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/gofast-live/gofast-cli/cmd/gofastv2/cmd"
+
+func main() {
+	cmd.Execute()
+}
--- v2/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/gofast-live/gofast-cli/v2/cmd"
-
-func main() {
-	cmd.Execute()
-}
